7e38f128d027077d56d4c8cfcdceda71aac4feae
abueesp <abueesp@gmail.com>
2018-05-31 22:32:16 +0100

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -963,7 +963,8 @@ sudo pacman -S opera opera-developer --noconfirm --needed
 sudo pacman -S vivaldi --noconfirm --needed
 
 #Chromium
-sudo pacman -S chromium --noconfirm --needed
+sudo pacman -S ungoogled-chromium --noconfirm --needed
+echo "alias chromium='ungoogled-chromium'" | tee -a $HOME/$USER/.bashrc
 #vim -c ":%s|google.com|ixquick.com|g" -c ":wq" ~/.config/chromium/Default/Preferences
 #vim -c ":%s|Google|Ixquick|g" -c ":wq" ~/.config/chromium/Default/Preferences
 #vim -c ":%s|yahoo.com|google.jp/search?q=%s&pws=0&ei=#cns=0&gws_rd=ssl|g" -c ":wq" ~/.config/chromium/Default/Preferences
