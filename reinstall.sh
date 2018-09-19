a56ab9d2ec7f05a4be46fadfe2fd5fc2e43cd8df
abueesp <abueesp@gmail.com>
2018-09-19 16:12:26 +0100

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -12,8 +12,9 @@
 #Script music (ready) media (ready) sec (ready) --- upload :)
 
 ### Restoring Windows on Grub2 ###
-sudo os-prober 
-if [ -n $(sudo os-prober) ]
+sudo os-prober
+GRUBPROBER=$(sudo os-prober)
+if [ -n "$GRUBPROBER" ]
             then
                         sudo grub-mkconfig -o /boot/grub/grub.cfg
             else
@@ -124,7 +125,7 @@ fi
 #echo 'alias chtor="sudo chroot --userspec=$TORUSER:$TORUSER /opt/torchroot /usr/bin/tor"' | tee -a .bashrc
 
 # Pacman over Tor
-sudo vim -c ':%s.#XferCommand = /usr/bin/curl.XferCommand = /usr/bin/curl --socks5-hostname localhost:$TORPORT -C - -f %u > %o" \n#XferCommand = /usr/bin/curl.g' -c ':wq' /etc/pacman.conf
+sudo vim -c ':%s.#XferCommand = /usr/bin/curl.XferCommand = /usr/bin/curl --socks5-hostname localhost:$TORPORT -C - -f %u > %o" \n.g' -c ':wq' /etc/pacman.conf
 
 # Running Tor in a systemd-nspawn container with a virtual network interface [which is more secure than chroot]
 TORCONTAINER=tor-exit #creating container and systemd service
