6301a9cf640a0f9041f5744c83ea8ab8d66b7d7d
abueesp <abueesp@gmail.com>
2018-10-17 04:30:43 +0100

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -943,6 +943,7 @@ cd ..
 #Network tools
 sudo pacman -S traceroute nmap arp-scan conntrack-tools --noconfirm --needed
 yaourt -S wireshark-cli wireshark-common wireshark-qt ostinato --noconfirm --needed
+sudo pacman -S hcxtools hcxdumptool hashcat --noconfirm --needed
 sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/sbin/dumpcap #wireshark permissions
 sudo  gpasswd -a $USER wireshark
 yaourt -S slurm nethogs --noconfirm #tops
